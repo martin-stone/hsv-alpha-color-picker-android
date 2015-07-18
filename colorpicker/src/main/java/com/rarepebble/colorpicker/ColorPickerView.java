e090a182c7f2889b392769bc4e51d9c45f5b50b9
Martin Stone <martin-stone@users.noreply.github.com>
2015-07-18 15:55:01 +0100

    Setting pointer position.

--- colorpicker/src/main/java/com/rarepebble/colorpicker/ColorPickerView.java
+++ colorpicker/src/main/java/com/rarepebble/colorpicker/ColorPickerView.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.FrameLayout;
 
 public class ColorPickerView extends FrameLayout implements HueSatView.HueSatListener {
+	private final HueSatView hueSatView;
+	private int color;
 	private final View swatchView;
 
 	public ColorPickerView(Context context) {
@@ -19,17 +21,27 @@ public class ColorPickerView extends FrameLayout implements HueSatView.HueSatLis
 		super(context, attrs);
 		LayoutInflater.from(context).inflate(R.layout.picker, this);
 
-		final HueSatView hueSatView = (HueSatView)findViewById(R.id.hueSatView);
+		hueSatView = (HueSatView)findViewById(R.id.hueSatView);
 		hueSatView.setChangeListener(this);
 
 		swatchView = findViewById(R.id.swatch);
 	}
 
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		hueSatView.setFromColor(color);
+	}
 
 	@Override
 	public void onHueSatChanged(float hue, float sat) {
 //		Log.e("hue", Float.toString(hue));
 //		Log.e("sat", Float.toString(sat));
-		swatchView.setBackgroundColor(Color.HSVToColor(new float[]{hue, sat, 1}));
+		color = Color.HSVToColor(new float[]{hue, sat, 1});
+		swatchView.setBackgroundColor(color);
 	}
+
+
 }
