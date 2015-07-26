14f563126d92f31501103d386fc1c6328392fa9e
Martin Stone <martin-stone@users.noreply.github.com>
2015-07-26 14:40:29 +0100

    Renamed app -> demo_app & integrated library.

---
+++ demo_app/src/main/java/com/rarepebble/colorpickerdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.rarepebble.colorpickerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.replace(android.R.id.content, new DemoPreferenceFragment())
+					.commit();
+		}
+	}
+
+	static public class DemoPreferenceFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preferences);
+		}
+	}
+}
