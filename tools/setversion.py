5d413be0ee3331d5c24222824a9867343a8d9c55
Martin Stone <martin-stone@users.noreply.github.com>
2015-09-05 18:18:17 +0100

    Version 1.3.1

---
+++ tools/setversion.py
@@ -0,0 +1,19 @@
+import re
+
+files = ["../README.md", "../colorpicker/build.gradle", "../demo_app/build.gradle"]
+
+new_version_code = "10301"
+new_version_string = "1.3.1"
+
+for file in files:
+    with open(file, "rb") as f:
+        old = f.read()
+    new = old
+    new = re.sub("versionCode \d+", "versionCode "+new_version_code, new)
+    new = re.sub("versionName '[\d.]+'", "versionName '{}'".format(new_version_string), new)
+    new = re.sub("libraryVersion = '[\d.]+'", "libraryVersion = '{}'".format(new_version_string), new) 
+    new = re.sub("compile 'com.rarepebble:colorpicker:[\d.]+'",
+        "compile 'com.rarepebble:colorpicker:{}'".format(new_version_string), new)
+
+    with open(file, "wb") as f:
+        f.write(new)
