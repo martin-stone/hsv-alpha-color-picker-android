662fb2098502fd35e637ee0574b01372f0e14146
Martin Stone <martin-stone@users.noreply.github.com>
2015-10-04 20:03:51 +0100

    Smooth corners on swatch.

--- colorpicker/src/main/java/com/rarepebble/colorpicker/SwatchView.java
+++ colorpicker/src/main/java/com/rarepebble/colorpicker/SwatchView.java
@@ -23,7 +23,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.view.View;
 
 public class SwatchView extends SquareView implements ObservableColor.Observer {
 
@@ -78,51 +77,67 @@ public class SwatchView extends SquareView implements ObservableColor.Observer {
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		// We expect to be stacked behind a square HueSatView and fill the top left corner.
+
 		final float inset = borderPaint.getStrokeWidth() / 2;
 		final float r = Math.min(w, h);
 
-		// We expect to be stacked behind a square HueSatView and fill the top left corner.
+		// Trim the corners of the swatch is where it is radialMarginPx thick.
+		// find how long the outeredges are:
 		final float margin = radialMarginPx;
 		final float diagonal = r + 2 * margin;
 		final float opp = (float)Math.sqrt(diagonal * diagonal - r * r);
 		final float edgeLen = r - opp;
-		final float innerX = edgeLen + margin * opp / diagonal;
-		final float innerY = margin * r / diagonal;
 
+		// Arc angles for drawing CCW
 		final float outerAngle = (float)Math.toDegrees(Math.atan2(opp, r));
 		final float startAngle = 270 - outerAngle;
-		final float sweepAngle = -90 + 2 * outerAngle;
+		// Sweep angle for each half of the swatch:
+		final float sweepAngle = outerAngle - 45;
+		// Sweep angle for the smooth corners:
+		final float cornerSweepAngle = 90 - outerAngle;
 
-		beginBorder(borderPath, inset, edgeLen, innerX, innerY);
-		arcTo(borderPath, r, margin, startAngle, sweepAngle);
-		endBorder(borderPath, inset, edgeLen, innerX, innerY);
+		// Outer border:
+		beginBorder(borderPath, inset, edgeLen, margin, cornerSweepAngle);
+		mainArc(borderPath, r, margin, startAngle, 2 * sweepAngle);
+		endBorder(borderPath, inset, edgeLen, margin, cornerSweepAngle);
 
+		// Old fill shape:
 		oldFillPath.reset();
 		oldFillPath.moveTo(inset, inset);
-		arcTo(oldFillPath, r, margin, 225, sweepAngle / 2);
-		endBorder(oldFillPath, inset, edgeLen, innerX, innerY);
+		mainArc(oldFillPath, r, margin, 225, sweepAngle);
+		endBorder(oldFillPath, inset, edgeLen, margin, cornerSweepAngle);
 
-		beginBorder(newFillPath, inset, edgeLen, innerX, innerY);
-		arcTo(newFillPath, r, margin, startAngle, sweepAngle / 2);
+		// New fill shape:
+		beginBorder(newFillPath, inset, edgeLen, margin, cornerSweepAngle);
+		mainArc(newFillPath, r, margin, startAngle, sweepAngle);
 		newFillPath.lineTo(inset, inset);
 		newFillPath.close();
 	}
 
-	private static void beginBorder(Path path, float inset, float edgeLen, float innerX, float innerY) {
+	private static void beginBorder(Path path, float inset, float edgeLen, float cornerRadius, float cornerSweepAngle) {
 		path.reset();
 		path.moveTo(inset, inset);
-		path.lineTo(edgeLen, inset);
-		path.lineTo(innerX, innerY);
+		cornerArc(path, edgeLen, inset, cornerRadius-inset, 0, cornerSweepAngle);
 	}
 
-	private static void endBorder(Path path, float inset, float edgeLen, float innerX, float innerY) {
-		path.lineTo(innerY, innerX);
-		path.lineTo(inset, edgeLen);
+	private static void endBorder(Path path, float inset, float edgeLen, float cornerRadius, float cornerSweepAngle) {
+		cornerArc(path, inset, edgeLen, cornerRadius - inset, 90 - cornerSweepAngle, cornerSweepAngle);
 		path.lineTo(inset, inset);
 		path.close();
 	}
-	private static void arcTo(Path path, float r, float margin, float startAngle, float sweepAngle) {
-		path.arcTo(new RectF(-margin, -margin, 2 * r + margin, 2 * r + margin), startAngle, sweepAngle);
+
+	private static void cornerArc(Path path, float cx, float cy, float r, float startAngle, float sweepAngle) {
+		final RectF ovalRect = new RectF(-r, -r, r, r);
+		ovalRect.offset(cx, cy);
+		path.arcTo(ovalRect, startAngle, sweepAngle);
+	}
+
+	private static void mainArc(Path path, float viewSize, float margin, float startAngle, float sweepAngle) {
+		float r = viewSize + margin;
+		final RectF ovalRect = new RectF(-r, -r, r, r);
+		ovalRect.offset(viewSize, viewSize);
+		path.arcTo(ovalRect, startAngle, sweepAngle);
 	}
 
 	protected void onDraw(Canvas canvas) {
